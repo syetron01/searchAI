5f5ac3345fc11ad1c0b2adcfa7846eedbc751641
Syetron1 <113598492+Syetron1@users.noreply.github.com>
2023-12-22 11:19:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAwMlqQ63_8YLE7Gm-HTeFxGjA8WMRkT10",
     authDomain: "searchai-a4b8d.firebaseapp.com",
@@ -12,10 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "301180826333",
     appId: "1:301180826333:web:75150fced3537601326134",
     measurementId: "G-5JBBEE3HW9",
-    databaseURL: "https://searchai-a4b8d-default-rtdb.asia-southeast1.firebasedatabase.app", // Update this line
+    databaseURL: "https://searchai-a4b8d-default-rtdb.asia-southeast1.firebasedatabase.app", 
   };
   
-  // Initialize Firebase
+
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 const database = getDatabase(app);
@@ -27,7 +25,6 @@ async function getDataFromFirebase() {
 
         if (snapshot.exists()) {
             const data = snapshot.val();
-            // Transform data into an array
             const dataArray = Object.keys(data).map(key => ({ ...data[key], id: key }));
             return dataArray;
         } else {
@@ -35,14 +32,12 @@ async function getDataFromFirebase() {
         }
     } catch (error) {
         console.error("Error fetching data:", error);
-        return []; // Ensure that you return a default value in case of an error
+        return [];
     }
 }
 
 document.getElementById("searchInput").addEventListener("keydown", function(event) {
-    // Check if the pressed key is Enter (key code 13)
     if (event.key === "Enter") {
-        // Call the search function
         search();
     }
 });
@@ -50,14 +45,12 @@ document.getElementById("searchInput").addEventListener("keydown", function(even
 async function search() {
     const searchInput = document.getElementById("searchInput").value.toLowerCase();
     const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = ""; // Clear previous results
-
+    resultDiv.innerHTML = ""; 
     const dataAi = await getDataFromFirebase();
 
     for (const entry of dataAi) {
         const keywords = entry.keywords.map(keyword => keyword.toLowerCase());
 
-        // Check if at least one word in the user input is present in at least one keyword for an entry
         const atLeastOneWordPresent = searchInput.split(' ').some(word => {
             return keywords.some(keyword => {
                 return keyword.includes(word);
@@ -78,8 +71,8 @@ async function search() {
             imageContainer.classList.add("image-container");
 
             const image = document.createElement("img");
-            image.src = getImagePath(entry.name); // Assuming a function to get the image path
-            image.alt = entry.keywords.join(", "); // Use all keywords as alt text
+            image.src = getImagePath(entry.name); 
+            image.alt = entry.keywords.join(", ");
 
             imageContainer.appendChild(image);
             entryContainer.appendChild(imageContainer);
@@ -98,7 +91,7 @@ async function search() {
 
             entryContainer.appendChild(textContainer);
 
-            resultDiv.appendChild(entryContainer);// Add a horizontal line between entries
+            resultDiv.appendChild(entryContainer);
             
 
         }
@@ -110,7 +103,6 @@ async function search() {
 }
 
 document.getElementById("searchButton").addEventListener("click", search);
-// Function to construct image path based on multiple keywords
 function getImagePath(name) {
 
   return `img/${name}.png`;
